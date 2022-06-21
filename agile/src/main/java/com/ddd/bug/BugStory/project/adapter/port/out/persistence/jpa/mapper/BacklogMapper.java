f94b7ee51fb820befc09fc5212ca7d0d1551fdb5
ozkanonay <ozkanonay@gmail.com>
2022-06-21 13:12:23 +0300

    migrated to monorepo

---
+++ agile/src/main/java/com/ddd/bug/BugStory/project/adapter/port/out/persistence/jpa/mapper/BacklogMapper.java
@@ -0,0 +1,17 @@
+package com.ddd.bug.BugStory.project.adapter.port.out.persistence.jpa.mapper;
+
+import com.ddd.bug.BugStory.project.adapter.port.out.persistence.jpa.entity.BacklogEntity;
+import com.ddd.bug.BugStory.project.domain.model.Backlog;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BacklogMapper {
+
+    BacklogMapper instance = Mappers.getMapper(BacklogMapper.class);
+
+
+    BacklogEntity domainToEntity(Backlog backlog);
+    Backlog entityToDomain(Backlog backlog);
+
+}
